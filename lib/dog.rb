06ec617023b01fc763f577585fe973947a1a2f57
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-29 23:44:09 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+#fido = Dog.new
+#fido.name = "Fido"
+#fido.bark - "woof!"
+ 
+#fido.name
+# => "Fido"
+ 
+#fido.bark
+#woof!
+# => nil
+
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+end
+end
+ 
+#lassie = Dog.new
+#lassie.name = "Lassie"
+  
+#puts lassie.name
+#fido.bark
