ec5fda41e6c509206645285e17c4fae48e7a3d0d
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-07-09 03:04:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,15 @@
-#fido = Dog.new
-#fido.name = "Fido"
-#fido.bark - "woof!"
- 
-#fido.name
-# => "Fido"
- 
-#fido.bark
-#woof!
-# => nil
-
-class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
-  end
+class Dog 
   
-  def bark
-    puts "woof!"
+def name=(name)
+  @name = name 
 end
+  
+def name 
+  @name 
+end 
+  
+def bark 
+  puts "woof!"
 end
- 
-#lassie = Dog.new
-#lassie.name = "Lassie"
-   
-#puts lassie.name
-#fido.bark
+  
+end
